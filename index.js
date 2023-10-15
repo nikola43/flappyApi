39eb7d8e822460e31dab46e4630fa2ffdcff73bd
Paulo Soares <pauloxti@gmail.com>
2023-10-15 13:28:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const cors = require('cors');
+const express = require('express');
+const dotenv = require('dotenv');
+const ethers = require('ethers');
+const packmanAbi = require('./packmanAbi.json');
+
+dotenv.config();
+const app = express();
+app.use(express.json())    // <==== parse request body as JSON
+
+const packmanAddress = "0x97f9f3D1061E5775EcdA2032647E16F81fF15762"
+const pvkey = process.env.PRIVATE_KEY;
+
+const provider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/polygon_mumbai");
+const wallet = new ethers.Wallet(pvkey, provider)
+const packmanContract = new ethers.Contract(packmanAddress, packmanAbi, wallet);
+
+app.use(cors({
+    //origin: 'https:website.com'
+    origin: '*'
+}));
+
+app.get('/', async (req, res) => {
+    res.send("Hello World");
+})
+
+app.post('/updateWinnerScore', async (req, res) => {
+    const winnerAddress = req.body.winnerAddress;
+    const winnerScore = req.body.winnerScore;
+    console.log(winnerAddress, winnerScore);
+
+    try {
+        const tx = await packmanContract.setPlayerScore(winnerAddress, winnerScore);
+        res.send({tx: tx.hash});
+    } catch (error) {
+        res.send(error.toString());
+    }
+})
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+})
